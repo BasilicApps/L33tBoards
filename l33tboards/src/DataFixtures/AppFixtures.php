a2f66465576f36898ed110c824a4f714f9842aaf
GGK <geegeek21@gmail.com>
2021-05-19 14:47:15 +0200

    Login and register
    Make sure to 
    php bin/console doctrine:migrations:migrate
    php bin/console doctrine:fixtures:load
    
    Templates still need to be done

--- l33tboards/src/DataFixtures/AppFixtures.php
+++ l33tboards/src/DataFixtures/AppFixtures.php
@@ -4,6 +4,8 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\User;
+use Faker;
 
 class AppFixtures extends Fixture
 {
@@ -11,6 +13,16 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        $faker = Faker\Factory::create();
+
+        for ($i = 0; $i < 3; $i++) {
+            $user = new User();
+            $user->setUsername($faker->firstName());
+            $user->setPassword($faker->stateAbbr()); //note that this is the hashed password so what is it doesn't really matter 
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
