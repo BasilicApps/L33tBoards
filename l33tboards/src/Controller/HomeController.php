30c653918723d4b70a9de782afa487c7542a99b3
GGK <geegeek21@gmail.com>
2021-06-13 18:02:40 +0200

    trendings posts

--- l33tboards/src/Controller/HomeController.php
+++ l33tboards/src/Controller/HomeController.php
@@ -4,24 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\PostRepository;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    private $posts;
+
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(PostRepository $postRepository): Response
     {
+        $this->postRepository = $postRepository;
+        $this->posts = $this->postRepository->find3MostTrending();
         if($this->getUser() != null){
             $boards = $this->getUser()->getFollowedBoards();
             dump($boards);
         }else{
             $boards = null;
         }
+        dump($this->posts);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'boards' => 'boards'
+            'boards' => $boards,
+            'trendingPosts' => $this->posts
+
         ]);
     }
 }
