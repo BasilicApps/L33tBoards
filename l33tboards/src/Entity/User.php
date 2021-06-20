b4726489d77134c8236a50aa7b991216560c83d9
GGK <geegeek21@gmail.com>
2021-06-20 18:34:59 +0200

    Comments (just need the form to be done :yey:)
    don't forget to migrate

--- l33tboards/src/Entity/User.php
+++ l33tboards/src/Entity/User.php
@@ -53,11 +53,17 @@ class User implements UserInterface
      */
     private $followedBoards;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="author", orphanRemoval=true)
+     */
+    private $comments;
+
     public function __construct()
     {
         $this->posts = new ArrayCollection();
         $this->boards = new ArrayCollection();
         $this->followedBoards = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -213,6 +219,36 @@ class User implements UserInterface
     public function removeFollowedBoard(Board $followedBoard): self
     {
         $this->followedBoards->removeElement($followedBoard);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setAuthor($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getAuthor() === $this) {
+                $comment->setAuthor(null);
+            }
+        }
 
         return $this;
     }
